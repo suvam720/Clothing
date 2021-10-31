881493d7a56018d3690b6da1dce9cd10801979c3
suvam720 <debnath.suvamhhh@gmail.com>
2021-10-31 09:18:45 +0530

    added contact page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-
+import ContactPage from './pages/contact/contact.component';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import{ setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
@@ -47,6 +47,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage}/>
+          <Route exact path='/contact' component={ContactPage} />
           <Route 
             exact 
             path='/signin' 
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
+import HomePage from "../homepage/homepage.component";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import './contact.styles.scss';
+
+
+
+const ContactPage = () => {
+
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [message, setMessage] = useState("");
+
+
+	return(
+
+	<div>
+		 <h1 className = 'title'>Contact with us</h1>
+		
+		<form className= 'contact'  onsubmit= "return false">
+                    <FormInput
+                      name = 'name'
+                      type = 'text' 
+                      label = 'Name'
+					  onChange = {e => setName({name: e.target.value})}
+                      required
+                      />
+					   <FormInput
+                      name = 'email'
+                      type = 'email' 
+                      label = 'email'
+					  onChange ={e => setEmail({email: e.target.value})}
+                      required
+                      />
+					   <FormInput
+                      name = 'text'
+                      type = 'text' 
+                      label = 'write your message'
+					  onChange  ={e => setMessage({message: e.target.value})}
+                      required
+                      />
+		<div className='button'>		  
+        <CustomButton type= 'submit' onClick = {
+       
+             <Redirect to = '/' />
+        }
+        
+         >Submit</CustomButton>
+		</div>	
+   </form>
+	</div>
+	)
+}
+
+
+export default ContactPage;
